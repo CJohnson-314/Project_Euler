b3b37fea2fa0bbf1d44c55540a48f9b1a6e06929
CJohnson-314 <89666779+CJohnson-314@users.noreply.github.com>
2024-06-11 21:35:38 +0100

    Add files via upload

--- ProjectEuler_3_factors_function.py
+++ ProjectEuler_3_factors_function.py
@@ -1,11 +1,12 @@
 import math
+from itertools import chain, combinations
 
 # for future use, save as a function
 def factors(n):
     #throw some errors
     assert n % 1 == 0, f"{n} is not an integer"
     assert n >= 0, f"{n} is negative"
-    assert n <= 10**16, f"{n} is too big, floats don't play nicely around this size"
+    assert n <= 10**12, f"{n} is too big, floats don't play nicely around this size"
     
     factors = [n] # start with the number itself as a factor
     prime_factors = []
@@ -15,32 +16,31 @@ def factors(n):
         prime_factors.append(2)
         return factors, prime_factors
     # if method can't find any factors, add to primes
-    # loop until prime_factors multiplies to n
-    #while math.prod(prime_factors) != factors[0]:
     for factor in factors:
         # deal with even numbers
         if factor == 2:
             prime_factors.append(2)
-            continue
         elif factor % 2 == 0:
             factors.append(2)
             factors.append(factor/2)
-            #prime_factors.append(2)
         # Fermat stuff
         else:
-            start = math.ceil(math.sqrt(factor))
+            start = math.ceil(factor**0.5)
             m = start**2 - factor
-            while math.sqrt(m) % 1 != 0:
+            while m**0.5 % 1 != 0:
                 start += 1
                 m = start**2 - factor
-            small_factor = start - math.sqrt(m)
-            big_factor = start + math.sqrt(m)
+            small_factor = start - m**0.5
+            big_factor = start + m**0.5
             if big_factor == factor: # for primes e.g. if factor = 7, big_factor = 7, append to primes
                 prime_factors.append(big_factor)
             else:
                 factors.append(small_factor)
-                factors.append(big_factor)
+                factors.append(big_factor) 
 
-    factors.append(1) # for completeness
+    prime_factors_subsets = list(chain(*[combinations(prime_factors,n) for n in range(len(prime_factors)+1)]))
+    for subset in prime_factors_subsets:
+        if math.prod(subset) not in factors:
+            factors.append(math.prod(subset))
 
-    return factors, prime_factors
+    return sorted(list(set(factors))), sorted(prime_factors)
