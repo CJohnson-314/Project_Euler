a9db4b69a1bc51c611c45c5ba5817b343dc9918a
cj <christopherwj2011@gmail.com>
2026-07-26 17:58:30 +0100

    Euler 53 solution

--- ProjectEuler_53.py
+++ ProjectEuler_53.py
@@ -3,14 +3,13 @@
 # How many values of n choose r are greater than 1 million,
 # for 1 <= r <= n <= 100
 # n choose r = n!/(r!(n-r)!)
-# Nature of that formula means there is a symetry around the halfway point
+# Nature of that formula means there is a symmetry around the halfway point
 # e.g 5C1 = 5C4, 5C2 = 5C3
 # So we only ever need to go up to halfway for each number
-# We can also use the symmetry of nCk
-# up to the halfway point it's strictly increasing
+# Furthermore, up to the halfway point it's strictly increasing
 # So, using the example from the question, 23C10 > 10^6
 # Implies that 23C11, 23C12, 23C13 > 10^6
-# Furthermore, we can use the fact that 100C3 < 10^6
+# Finally, we can use the fact that 100C3 < 10^6
 # so never need to check 1/2
 
 from math import comb
